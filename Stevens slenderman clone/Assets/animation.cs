5d98c70cffc2555d9c223021c6345d5a91c3eccb
ShadowMuffin98 <62254811+ShadowMuffin98@users.noreply.github.com>
2020-03-30 13:22:40 -0400

    New room and made the tv have static and fixed stair slope

---
+++ Stevens slenderman clone/Assets/animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour
+{
+    public List<Texture> frames;
+    public Renderer renderer;
+    int frames_count = 0;
+    float timer= 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTexture = frames[frames_count];
+        renderer.material.SetTexture("_EmissionMap", frames[frames_count]);
+
+        timer += Time.deltaTime;
+        if (timer > 0.1f)
+        {
+            timer = 0;
+            frames_count += 1;
+            if (frames_count > 17)
+            {
+                frames_count = 0;
+            }
+        }
+    }
+}
